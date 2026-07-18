db0f53321fa1ab965d644424b876614d48e09d42
sohail <sohailsadiqshaik@gmail.com>
2026-07-18 12:54:36 +0530

    redis configuration added

--- src/main/java/com/aimarketplace/config/RedisConfig.java
+++ src/main/java/com/aimarketplace/config/RedisConfig.java
@@ -1,9 +1,16 @@
 package com.aimarketplace.config;
 
 
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+import java.time.Duration;
 
 
 @Configuration
@@ -11,5 +18,14 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @EnableRedisRepositories
 public class RedisConfig {
 
+    @Bean
+    public CacheManager cm(RedisConnectionFactory rf){
+        RedisCacheConfiguration rcc=RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMillis(5000));
+
+        return RedisCacheManager.builder(rf)
+                .cacheDefaults(rcc)
+                .build();
+    }
 
 }
