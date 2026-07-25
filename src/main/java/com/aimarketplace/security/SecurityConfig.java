1921f8d2ac57833b03fd18191fe494877ab3839a
sohail <sohailsadiqshaik@gmail.com>
2026-07-26 00:47:03 +0530

    added actuator

--- src/main/java/com/aimarketplace/security/SecurityConfig.java
+++ src/main/java/com/aimarketplace/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 .authorizeHttpRequests(auth -> auth
-
+                        .requestMatchers("/actuator/health").permitAll()
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
